21201159d54288702f0055ed672848ccf23dc6c9
FremyUwU <endergosha660@yandex.ru>
2024-12-30 20:48:29 +0500

    batch

--- src/main/java/com/george/springcourse/dao/PersonDAO.java
+++ src/main/java/com/george/springcourse/dao/PersonDAO.java
@@ -30,7 +30,7 @@ public class PersonDAO {
                 .stream().findAny().orElse(null);
     }
     public void save(Person person) {
-        jdbcTemplate.update("INSERT INTO Person VALUES(1, ?, ?, ?)", person.getName(), person.getAge(), person.getEmail());
+        jdbcTemplate.update("INSERT INTO Person(name, age, email) VALUES(?, ?, ?)", person.getName(), person.getAge(), person.getEmail());
     }
     public void update(int id, Person updatedPerson) {
         jdbcTemplate.update("UPDATE Person SET name=?, age=?, email=? WHERE id=?", updatedPerson.getName(), updatedPerson.getAge(), updatedPerson.getEmail(), id);
